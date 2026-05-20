dc6d1eb64267e41c6c2e6a59b7b9ae9b104428fe
willieatc <willieatc@gmail.com>
2026-05-20 20:13:10 +0800

    Update config.js

--- config.js
+++ config.js
@@ -9,5 +9,5 @@
  */
 
 window.ATC_CONFIG = {
-    SHEETS_WEB_APP_URL: "請貼上你的 Google Apps Script Web App URL"
+    SHEETS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbxMFkEY5ApRPOoisnCePL96XNLvbYwzpB99Eh_x-3doOF00NvYk0YKMhEjuSFIXXIaz/exec"
 };
