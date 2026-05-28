66a46f983b49e0254966f6bfeb552a1cedc3eb1f
willieatc <willieatc@gmail.com>
2026-05-28 16:20:33 +0800

    Update config.js

--- config.js
+++ config.js
@@ -9,5 +9,5 @@
  */
 
 window.ATC_CONFIG = {
-    SHEETS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzmpPHuE5tStQI88Gt2vD0M64dtjnmkY46hWD_yF2t3nR1GEZl2Ckx9snqrV7g6v5vOUQ/exec"
+    SHEETS_WEB_APP_URL: "https://script.google.com/macros/s/AKfycbxDu99KTyhe6sL9Nn60VgZkhbme166zYh-mZNDz0D5LDofvO-lIAWG-yc1sQGtJkgzLZw/exec"
 };
